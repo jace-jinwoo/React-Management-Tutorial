374a9e9f8558067d30454e0a3943fc4bd0f6d854
jinwoo <jinwoopark@devrock.co.kr>
2020-07-24 19:07:24 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 import Paper from '@material-ui/core/Paper'
@@ -21,36 +21,25 @@ const styles = theme => ({
 })
 
 
-const customers = [{
-  id: 1,
-  image: 'https://placeimg.com/64/64/any',
-  name: '박진우',
-  birthday: '911226',
-  gender: '남자',
-  job: '웹퍼블리셔'
-},
-{
-  id: 2,
-  image: 'https://placeimg.com/64/64/any?t=1595578610444',
-  name: '김남영',
-  birthday: '970213',
-  gender: '여자',
-  job: '디자이너'
-},
-{
-  id: 3,
-  image: 'https://placeimg.com/64/64/any?t=1595578633614',
-  name: 'Who',
-  birthday: '911226',
-  gender: '남자',
-  job: '축구선수'
-}]
-
 function App(props) {
-  const {classes} = props;
+  
+  const [state, setState] = useState({customers: ""})
+
+  useEffect(() => {
+    callApi()
+    .then(res => setState({customers: res}))
+    .catch(err => console.log(err));
+  })
+  
+  const callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
+    <Paper className={props.root}>
+      <Table className={props.table}>
         <TableHead>
           <TableRow>
             <TableCell>번호</TableCell>
@@ -62,7 +51,7 @@ function App(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) }) } 
+          {state.customers ? state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) }) : '' } 
         </TableBody>
       </Table>
     </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-  res.send({message: 'Hello Express!'});
-});
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: 'https://placeimg.com/64/64/any',
+      name: '박진우',
+      birthday: '911226',
+      gender: '남자',
+      job: '프론트엔드'
+    },
+    {
+      id: 2,
+      image: 'https://placeimg.com/64/64/any?t=1595578610444',
+      name: '김남영',
+      birthday: '970213',
+      gender: '여자',
+      job: '디자이너'
+    },
+    {
+      id: 3,
+      image: 'https://placeimg.com/64/64/any?t=1595578633614',
+      name: 'Who',
+      birthday: '911226',
+      gender: '남자',
+      job: '축구선수'
+    }
+  ]);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
